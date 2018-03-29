36a4defb3d5f0000169a09fc68ce4d666798f155
camilabelen <ccornejoss@gmail.com>
2018-03-28 21:47:20 -0300

    prueba 2 php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,28 +1,20 @@
 <?php 
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $insitution = $_POST['institution'];
-  $position = $_POST['position'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+  
+  if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $insitution = $_POST['institution'];
+    $position = $_POST['position'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  $email_form = 'ccornejoss@gmail.com';
+    $mailTo = "levineridesigner@outlook.com";
+    $headers = "From: ".$email;
+    $txt = "You have received and e-mail from ".$name.".\n\n".$message;
 
-  $email_subject = "New Form Submission";
+    mail($mailTo,$subject,$txt,$headers);
 
-  $email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "User Institution: $institution.\n".
-                "User Position: $position.\n".
-                "Email Subject: $subject.\n".
-                "User Message: $message.\n";
-
-  $to = "ccornejoss@gmail.com";
-
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-to: $visitor_email\r\n";
-
-  mail($to,$email_subject,$email_body,$headers);
-  header("Location: index.html");
-
+    header("Location: index.html?mailsend");
+  }
+  
 ?>
